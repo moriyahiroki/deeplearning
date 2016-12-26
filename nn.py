cd574bd3f324f63d132ad1e51af71764d1eb2bd0
moriyahiroki <hiroki.moriya.nx@gmail.com>
2016-12-26 16:21:41 +0900

    first commit

---
+++ nn.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5],[0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+print(W1.shape)
+print(X.shape)
+print(B1.shape)
+
+A1 = np.dot(X, W1) + B1
+print(A1)
+
+Z1 = sigmoid(A1)
+
+print(Z1)
+
